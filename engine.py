7056496cb804706930ac3ba9d9931e041ec4362e
dongzeyuan <dongfujing88@gmail.com>
2018-10-17 21:34:31 +0800

    fix dll

--- engine.py
+++ engine.py
@@ -264,7 +264,7 @@ def play_game(player, entities, game_map, message_log, game_state, con, panel, c
 def main():
     constants = get_constants()
 
-    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    libtcod.console_set_custom_font('rogue-tutorial\\fonts\\arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
     libtcod.console_init_root(constants['screen_width'], constants['screen_height'], constants['window_title'], False)
 
